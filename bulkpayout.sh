356288c8a85ea9c76c67acd0dfbc8547ab33cb92
Guinnessstache <48448154+Guinnessstache@users.noreply.github.com>
2021-11-01 13:23:20 -0400

    Update bulkpayout.sh

--- bulkpayout.sh
+++ bulkpayout.sh
@@ -1,3 +1,3 @@
-curl -X GET "https://othub-api.origin-trail.network/api/nodes/DataHolders/0xCHANGEMETOYOURerc725ID" -H "accept: text/plain" | grep -o -P '(?<=OfferId":").*?(?=","FinalizedTimestamp")' > offerids.txt
+curl -X GET "https://v5api.othub.info/api/nodes/DataHolder/ReplaceNodeID/jobs" -H "accept: text/plain" | grep -o -P '(?<=OfferId":").*?(?=","FinalizedTimestamp")' > offerids.txt
 
 cat offerids.txt | while read line; do docker exec otnode curl -s -X GET http://127.0.0.1:8900/api/latest/payout?offer_id=$line; done
